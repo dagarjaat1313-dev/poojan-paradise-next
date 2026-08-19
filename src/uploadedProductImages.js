abdd9368a1fc0ded44d5d293c499f8f40d845e80
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-08-19 21:06:52 +0530

    Remove temporary placeholder image data

--- src/uploadedProductImages.js
+++
@@ -1,6 +0,0 @@
-export const uploadedProductImages = {
-  'honey': 'data:image/webp;base64,UklGRiQ2AABXRUJQVlA4IBQ2AABwAgCdASoUAAQAAkA4JZQCdAD0w8QAA/vuUAAA=',
-  'peela-kapda': 'data:image/webp;base64,UklGRiQ2AABXRUJQVlA4IBQ2AABwAgCdASoUAAQAAkA4JZQCdAD0w8QAA/vuUAAA=',
-  'gobar-cups-diya': 'data:image/webp;base64,UklGRiQ2AABXRUJQVlA4IBQ2AABwAgCdASoUAAQAAkA4JZQCdAD0w8QAA/vuUAAA=',
-  'pooja-ghee': 'data:image/webp;base64,UklGRiQ2AABXRUJQVlA4IBQ2AABwAgCdASoUAAQAAkA4JZQCdAD0w8QAA/vuUAAA='
-};
